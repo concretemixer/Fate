d5ef798b62fcc572239c9fe0f2e6f484f9db0672
max <max.pervukhin@gmail.com>
2016-08-11 02:20:35 +0700

    upd

---
+++ Assets/ScenarioMainSquare.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Fate {
+    public class ScenarioMainSquare : Scenario {
+
+    	// Use this for initialization
+    	void Start () {
+            state = State.Playing;    	
+    	}
+    	
+    	// Update is called once per frame
+    	void Update () {
+    	
+    	}
+    }
+}
